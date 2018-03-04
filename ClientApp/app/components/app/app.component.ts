98f9c5138649002c3725168040a50c5f5836c8b0
Dac Toan Ho <dac@cae.com>
2018-03-04 14:21:14 -0500

    Add hub

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+    ngOnInit() {
+       
+        //this._hubConnection = new HubConnection('http://localhost:5001/hubbus');
+
+        //this._hubConnection
+        //    .start()
+        //    .then(() => console.log('Connection started!'))
+        //    .catch(err => console.log('Error while establishing connection :('));
+
+        //this._hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
+        //    const text = `${nick}: ${receivedMessage}`;
+        //    this.messages.push(text);
+        //});
+
+    }
 }
--- README.md
+++ README.md
@@ -11,6 +11,8 @@
     * dotnet restore
 * Install packages
     * npm install
+* Install SignalR 
+	* dotnet add package Microsoft.AspNetCore.SignalR --version 1.0.0-alpha2-final 
 
 ## Open in VS 2017
 * start SenegalBus.csproj
